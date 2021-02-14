c1c59d4ad979a384faf5a48c594fc328b1d88b72
rennelou <rennelou@outlook.com>
2021-02-14 16:22:51 -0300

    get abcs

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub mod waveguide {
 	
 	use num::complex::Complex;
 
-	const MINIMALSTEP: u32 = 5;
+	const MINIMALSTEP: i64 = 5;
 
 	pub struct Slab {
 		xsteps: i64,
@@ -17,7 +17,7 @@ pub mod waveguide {
 		q: Vec<Vec<Complex<f64>>>,
 	}
 
-	pub struct Abc {
+	struct Abc {
 		a: Complex<f64>,
 		b: Complex<f64>,
 		c: Complex<f64>,
@@ -61,5 +61,70 @@ pub mod waveguide {
 	    		q:      q,
 	    	}
 	    }
+
+		fn get_abcs(&self, z: usize) -> Vec<Abc> {
+			let mut result: Vec<Abc> = Vec::new();
+			
+			if self.xsteps >= MINIMALSTEP {
+				result.push(Abc {
+					// okamoto 7.108a
+					a: Complex::new(0.0, 0.0), 
+					b: self.s[z][1] - self.left_boundary(z), 
+					c: Complex::new(1.0, 0.0)
+				});
+
+				result.append(&mut (2..self.xsteps-2).map(
+					// okamoto 7.108b
+					|i| Abc {
+						a: Complex::new(1.0, 0.0), 
+						b: self.s[z][i as usize], 
+						c: Complex::new(1.0, 0.0)
+					}
+				).collect());
+
+				result.push(Abc {
+					/// okamoto 7.108c
+					a: Complex::new(1.0, 0.0), 
+					b: self.s[z][(self.xsteps - 2) as usize] - self.right_boundary(z), 
+					c: Complex::new(0.0, 0.0)
+				});
+			}
+
+			return result;
+		}
+
+		fn right_boundary(&self, _z: usize) -> Complex<f64> {
+			return ( 
+				Complex::new(0.0, -1.0)*self.kright*Complex::new(self.xdelta, 0.0) 
+			).exp();
+		}
+
+		fn left_boundary(&self, _z: usize) -> Complex<f64> {
+			return ( 
+				Complex::new(0.0, -1.0)*self.kleft*Complex::new(self.xdelta, 0.0) 
+			).exp();
+		}
 	}
-}	
+
+	#[cfg(test)]
+	mod tests {
+		use super::*;
+    	use num::complex::Complex;
+
+    	#[test]
+    	fn assert_abcs_sizes() {
+    	    for i in 1..10 {
+    	        let w = get_waveguide_mock(100.0, i as f64, 2.0, 1.0, 1.0/1550.0, 3.4757, 1.0, 0.2, Complex::new(0.0, 0.0), Complex::new(0.0, 0.0));
+				let got = w.get_abcs(0);
+
+				assert_eq!(got.len(), (w.xsteps-2) as usize );
+    	    }
+    	}
+
+    	fn get_waveguide_mock(dx: f64, xdelta: f64, dz: f64, zdelta: f64,
+    		k: f64, n: f64, n0: f64, alpha: f64, kleft: Complex<f64>, kright: Complex<f64>) -> Slab {
+			
+    	    return Slab::new(dx, xdelta, dz, zdelta, k, n, n0, alpha, kleft, kright);
+    	}
+	}
+}
--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -1,18 +1,4 @@
 #[cfg(test)]
 mod tests {
-    use rust_fdmbpm::waveguide;
-    use num::complex::Complex;
-
-    #[test]
-    fn assert_abcs_sizes() {
-        for i in 1..10 {
-            let _ = get_waveguide_mock(100.0, i as f64, 2.0, 1.0, 1.0/1550.0, 3.4757, 1.0, 0.2, Complex::new(0.0, 0.0), Complex::new(0.0, 0.0));
-        }
-    }
-
-    fn get_waveguide_mock(dx: f64, xdelta: f64, dz: f64, zdelta: f64,
-    	k: f64, n: f64, n0: f64, alpha: f64, kleft: Complex<f64>, kright: Complex<f64>) -> waveguide::Slab {
     
-        return waveguide::Slab::new(dx, xdelta, dz, zdelta, k, n, n0, alpha, kleft, kright);
-    }
 }
