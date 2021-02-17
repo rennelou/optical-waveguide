209a8ae4d75d1c83f5d3c69929c884d2e03ef4e8
rennelou <rennelou@outlook.com>
2021-02-17 02:03:34 -0300

    começando a plottar graficos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use rust_fdmbpm::waveguide::slab;
+use num::complex::Complex;
+use plotters::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let w = slab::mock::get_waveguide_mock(10.0, 2.0, 10.0, 1.0, 1.0/1550.0, 3.4757, 1.0, 0.2, Complex::new(1.0, 0.0), Complex::new(1.0, 0.0));
+	let fdmbpm = w.fdmbpm(slab::mock::get_ones(5));
+
+    let root_drawing_area = BitMapBackend::new("0.1.png", (1024, 768))
+        .into_drawing_area();
+
+    root_drawing_area.fill(&WHITE).unwrap();
+
+    let mut chart = ChartBuilder::on(&root_drawing_area)
+        .build_cartesian_2d(0.0..10.0, 0.0..1.0)
+        .unwrap();
+
+    for (i,l) in fdmbpm.iter().enumerate() {
+        let offset = i as f64 / 10.0;
+        chart.draw_series(LineSeries::new(
+            l.iter().enumerate().map(|(i,x)| (i as f64, x.norm()+offset)),
+            &RED
+        )).unwrap();
+    }
 }
