7e1be5ce080d3620084e3d454917317b8a7bdc51
rennelou <rennelou@outlook.com>
2021-02-21 15:38:43 -0300

    cleaning

--- src/main.rs
+++ src/main.rs
@@ -23,14 +23,14 @@ fn main() {
         .caption("Waveguide", ("Arial", 30))
         .set_label_area_size(LabelAreaPosition::Left, 40)
         .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .build_cartesian_2d(0.0..10.0, 0.0..22.0)
+        .build_cartesian_2d(0.0..10.0, 0.0..21.0)
         .unwrap();
 
     chart.configure_mesh().draw().unwrap();
 
-    for (offset, l) in z_points.iter().cloned().zip(fdmbpm) {
+    for (offset, l) in z_points.iter().copied().zip(fdmbpm) {
         chart.draw_series(LineSeries::new(
-            x_points.iter().cloned().zip(
+            x_points.iter().copied().zip(
                    l.iter().map(|c|{
                         let (r, theta) = c.clone().to_polar();
                         offset + (r * theta.cos())
@@ -52,5 +52,5 @@ fn gaussian(points: &List<f64>, mean: f64, sigma: f64) -> List<f64> {
 }
 
 fn f64_to_complex(l: List<f64>) -> List<Complex<f64>> {
-    return l.iter().cloned().map(|x|Complex::new(x, 0.0)).collect();
+    return l.iter().copied().map(|x|Complex::new(x, 0.0)).collect();
 }
