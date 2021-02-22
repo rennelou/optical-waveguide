340dfe66b3e4c574a2a6d0f7582c1bb646f362c5
rennelou <rennelou@outlook.com>
2021-02-21 21:41:46 -0300

    cleaning

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rust_fdmbpm::waves::gaussian;
+use rust_fdmbpm::waves;
 use rust_fdmbpm::fp::list::List;
 use rust_fdmbpm::waveguide::slab;
 use rust_fdmbpm::plotters;
@@ -13,9 +13,9 @@ fn main() {
 
     let w = slab::new(dx, xdelta, dz, zdelta, 1.0/1550.0, 3.4757, 3.4757, 0.0, Complex::new(1.0, 0.0), Complex::new(1.0, 0.0));
     
-    let normal = gaussian(w.get_x_points(), 5.0, 0.2);
+    let gaussian = waves::gaussian(w.get_x_points(), 5.0, 0.2);
     
-    let es_2d = w.fdmbpm(f64_to_complex(normal));
+    let es_2d = w.fdmbpm(f64_to_complex(gaussian));
 
     plotters::plot_waveguide_2d(es_2d, dx, dz + 1.0);
 }
