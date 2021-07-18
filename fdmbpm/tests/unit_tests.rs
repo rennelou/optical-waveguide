40fe15dc05953f8c7072d68ae53a1c38737c72f6
rennelou <rennelou@outlook.com>
2021-07-18 19:16:40 -0300

    teste de gaussian 4 um

--- fdmbpm/tests/unit_tests.rs
+++ fdmbpm/tests/unit_tests.rs
@@ -1,54 +1,51 @@
 #[cfg(test)]
 mod tests {
+    use rust_fdmbpm::tools;
     use rust_fdmbpm::waveguide::cores;
 	use rust_fdmbpm::waveguide::fdmbpm;
 	use rust_fdmbpm::waveguide::boundary_codition;
 	use rust_fdmbpm::waves;
 	use core::f64::consts::PI;
-	use ndarray::Array;
 	use std::error::Error;
 
     #[test]
-   	fn slab() -> Result<(), Box<dyn Error>> {
-		let k0 = (2.0*PI)/1.55e-6_f64;
-
-		let xdepht = 1024usize;
-		let zdepht = 1000usize;
-
-    	let dx = 260e-6 * k0;
+   	fn core_8_gaussian_4() -> Result<(), Box<dyn Error>> {
+		let k0 = 2.0*PI/1.15;
+    
+		let xdepht = 2000usize;
+		let dx = 40.0;
     	let xdelta = dx/(xdepht as f64);
 		
-    	let zdelta = 0.5e-6 * k0;
-    	let dz = zdelta * (zdepht as f64);
-
-    	let position = dx/2.0;
-    	let width = 35e-6 * k0;
-
+    	let dz = 750.0;
+    	let zdelta = 0.5;
+		
+		let position = dx/2.0;
+    	let width = 8.0;
+		
 		let shape = [xdepht];
 		let deltas = [xdelta];
 		let center = [position];
-
-    	let n0 = 3.0;
-    	let n = 3.3;
+		
+		let n0 = 3.377;
+    	let n = 3.38;
 
     	let core = cores::rectilinear::new_2d(dx, xdelta, dz, zdelta, n, n0, position, width);
 		
-    	let p = 200.0;
-    	let eta = 120.0 * PI; // eta usa eps e mi do meio
-    	let w = 10e-6 * k0;
-    	let e0 = p*eta / (w.powf(2.0)*PI);
-    	let gaussian = waves::gaussian(&shape, &deltas, &center, e0, w);
+    	let w = 2.0_f64;
+		let gaussian = waves::gaussian(&shape, &deltas, &center, 1.0, w);
+		
+		let e = fdmbpm::slab2d::run(&core, k0, 0.0, gaussian, boundary_codition::transparent);
+		let result = (e.get_intensity(), e.shape().clone());
 
-    	let e = fdmbpm::slab2d::run(&core, 1.0, 0.0, gaussian, boundary_codition::dirichlet);
-		// para gerar seria so exportar e -- export::hdf5("example.h5", &e, &core);
-
-		let intensity = e.get_intensity();
-    	let array = Array::from_shape_vec(e.shape().clone(), intensity)?;
-
-    	let file = hdf5::File::open("slab.h5")?;
-		let values = file.dataset("intensity")?;
-
-		assert_eq!(values.read_dyn::<f64>()?, array);
+    	let file = hdf5::File::open("tests/datas/core_8_gaussian_4.h5")?;
+		let reference = file.dataset("intensity").unwrap();
+		
+		let diffs = tools::areas_diff(
+			tools::normalize(tools::dataset_to_matrix(reference)), 
+			tools::normalize(result)
+		);
+		
+		assert!(diffs.into_iter().all(|x| x <= 0.011)); // erro de ate 1.1%
 
 		Ok(())
    	}
