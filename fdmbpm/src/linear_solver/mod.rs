e3d614acc0bc082977eb04b008ef69c5db82de59
rennelou <rennelou@outlook.com>
2021-07-27 22:30:03 -0300

    diagonal_matrix

--- fdmbpm/src/linear_solver/mod.rs
+++ fdmbpm/src/linear_solver/mod.rs
@@ -1,3 +1,4 @@
+use crate::fp::{self, list};
 use num::Complex;
 
 pub mod thomas;
@@ -10,4 +11,30 @@ const fn zero() -> &'static Complex<f64> {
 const COMPLEX_ONE: Complex<f64> = Complex::new(1.0, 0.0);
 const fn one() -> &'static Complex<f64> {
 	&COMPLEX_ONE
+}
+
+pub struct DiagonalMatrix {
+	pub below_diag: Vec<Complex<f64>>,
+    pub diag: Vec<Complex<f64>>,
+    pub above_diag:Vec<Complex<f64>>,
+
+    pub lenght: usize
+}
+
+pub fn diagonal_matrix(below_diag: Vec<Complex<f64>>, diag: Vec<Complex<f64>>, above_diag: Vec<Complex<f64>>) -> DiagonalMatrix {
+    if below_diag.len() != diag.len() || above_diag.len() != diag.len() {
+		panic!("all input vectors must have the sames lenghts")
+	}
+
+    let lenght = below_diag.len();
+	
+	if fp::head(below_diag.iter()).unwrap() != zero() {
+		panic!("before_diagonal vector must init with 0")
+	}
+
+	if fp::last(above_diag.iter()).unwrap() != zero() {
+		panic!("above_diagonal vector must ends with 0")
+	}
+
+    DiagonalMatrix { below_diag, diag, above_diag, lenght }
 }
--- fdmbpm/src/linear_solver/thomas.rs
+++ fdmbpm/src/linear_solver/thomas.rs
@@ -1,23 +1,13 @@
 use super::*;
 use itertools::izip;
-use crate::fp::{self, list};
 
-pub fn try_solve(below_diag: Vec<Complex<f64>>, diag: Vec<Complex<f64>>, above_diag: Vec<Complex<f64>>, const_terms: Vec<Complex<f64>>) -> Vec<Complex<f64>> {
-	
-	if below_diag.len() != diag.len() || above_diag.len() != diag.len() || const_terms.len() != diag.len() {
-		panic!("all input vectors must have the sames lenghts")
-	}
-	
-	if fp::head(below_diag.iter()).unwrap() != zero() {
-		panic!("before_diagonal vector must init with 0")
-	}
-
-	if fp::last(above_diag.iter()).unwrap() != zero() {
-		panic!("above_diagonal vector must ends with 0")
-	}
-
-	solve(below_diag, diag, above_diag, const_terms)
-
+// m*x = const_terms
+pub fn try_solve(m: DiagonalMatrix, const_terms: Vec<Complex<f64>>) -> Vec<Complex<f64>> {
+	if m.lenght == const_terms.len() {
+        solve(m.below_diag, m.diag, m.above_diag, const_terms)
+    } else {
+        panic!("constant terms must have the same lenght that diagonal matrix")
+    }
 }
 
 fn solve(below_diag: Vec<Complex<f64>>, diag: Vec<Complex<f64>>, above_diag: Vec<Complex<f64>>, const_terms: Vec<Complex<f64>>) -> Vec<Complex<f64>> {
@@ -63,8 +53,10 @@ mod tests {
         let above_diag = vec![cplx(-1.0), cplx(-1.0), cplx(-1.0), cplx(-1.0), cplx(0.0)];
         let const_terms = vec![cplx(0.0), cplx(0.0), cplx(0.0), cplx(0.0), cplx(1.0)];
         
+        let m = diagonal_matrix(below_diag, diag, above_diag);
+
         let expected = vec![cplx(1.0), cplx(2.0), cplx(3.0), cplx(4.0), cplx(5.0)];
-        let result = try_solve(below_diag, diag, above_diag, const_terms);
+        let result = try_solve(m, const_terms);
         assert!(izip!(&expected,&result).all( |(e, r)| (e-r).norm() < 1e-6 ));
     }
 
