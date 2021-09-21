f3e74135160a4ccd826d45c178f8a495c2268555
rennelou <rennelou@outlook.com>
2021-09-21 16:33:12 -0300

    explicitando no codigo o metodo da direcao implicita alternada

--- optical_waveguide/src/fdmbpm/slab/tridimensional.rs
+++ optical_waveguide/src/fdmbpm/slab/tridimensional.rs
@@ -1,8 +1,8 @@
+use crate::fp::Matrix;
+
 use super::*;
 use eletric_field::EletricField;
 use fp::list;
-
-//Nome de funções e variaveis precisa enfatizar o algoritmo da direção implicita
 
 impl Slab<3,2> {
 	
@@ -17,73 +17,8 @@ impl Slab<3,2> {
 			|result, z| {
 				
 				let last_es = fp::last(result.iter()).unwrap();
-	
-				let d_plane: Vec<Vec<_>> = (1..xdepht-1).map(
-					|x| {
-						let mut q = vec![];
-						let mut es = vec![];
-
-						for y in 0..ydepht {
-							q.push(self.get_q(ydelta, [z-1, y, x]));
-							es.push(last_es.get(&[y, x]).clone())
-						}
-		
-						get_const_terms(&es,q).into_iter().map(|d| d * self.dx2bydy2()).collect()
-				}).collect();
-
-				let mut e_intermediate = vec![];
-				let mut h_plane: Vec<Vec<_>>  = vec![];
-				for y in 1..ydepht-1 {
-			
-					let mut s = vec![];
-					let mut q = vec![];
-					let mut es = vec![];
-					let mut d = vec![];
-
-					for x in 0..xdepht {
-						q.push(self.get_q(xdelta, [z-1, y, x]));
-						
-						if x > 0 && x < xdepht-1 {
-							s.push(self.get_s(xdelta, [z-1, y, x]));
-						
-							es.push(last_es.get(&[y, x]).clone());
-							d.push(d_plane[y-1][x-1]);
-						}
-					}
-
-					let e = self.get_es(self.equation_to_diagonal_matrix(s, &es), d);
-					let h = get_const_terms(&e ,q).into_iter().map(|d| d * self.dy2bydx2()).collect();
-
-					e_intermediate.push(e);
-					h_plane.push(h);
-				}
-
-				let e_transposed: Vec<Vec<_>> = (1..xdepht-1).map(|x|{
-					let mut s = vec![];
-					let mut es = vec![];
-					let mut d = vec![];
-
-					for y in 1..ydepht-1 {
-						s.push(self.get_s(ydelta, [z-1, y, x]));
-						es.push(e_intermediate[y-1][x]);
-						d.push(h_plane[y-1][x-1]);
-					}
-
-					self.get_es(self.equation_to_diagonal_matrix(s, &es), d)
-
-				}).collect();
-				let es_middle = matrix::transposed_vec2_to_matrix2(e_transposed);
-
-				let es = (0..ydepht).map(
-					|y| {
-						let mut e_middle_line = vec![];
-						for x in 0..xdepht-2 {
-							e_middle_line.push(es_middle.get(&[y,x]).clone());
-						}
-						self.insert_boundary_values(e_middle_line)
-					}
-				).collect();
-				let e = matrix::vec2_to_matrix2(es);
+				
+				let e = self.alternate_direction_implicit_method(last_es, z);
 
 				list::append(result, e)
 			}
@@ -92,6 +27,80 @@ impl Slab<3,2> {
 		eletric_field::new (matrix::merge(es), self.grid.get_deltas().to_vec())
 	}
 	
+	fn alternate_direction_implicit_method(&self, last_es: &Matrix<Phasor>, z: usize) -> Matrix<Phasor> {
+		let &[_, ydepht, xdepht] = self.grid.get_shape();
+		let &[_, ydelta, xdelta] = self.grid.get_deltas();
+
+		let const_y_explicit_plane: Vec<Vec<_>> = (1..xdepht-1).map(
+			|x| {
+				let mut q = vec![];
+				let mut es = vec![];
+
+				for y in 0..ydepht {
+					q.push(self.get_q(ydelta, [z-1, y, x]));
+					es.push(last_es.get(&[y, x]).clone())
+				}
+
+				get_const_terms(&es,q).into_iter().map(|d| d * self.dx2bydy2()).collect()
+		}).collect();
+
+		let mut e_x_expicit = vec![];
+		let mut const_x_explicit_plane: Vec<Vec<_>>  = vec![];
+		for y in 1..ydepht-1 {
+	
+			let mut s = vec![];
+			let mut q = vec![];
+			let mut es = vec![];
+			let mut d = vec![];
+
+			for x in 0..xdepht {
+				q.push(self.get_q(xdelta, [z-1, y, x]));
+				
+				if x > 0 && x < xdepht-1 {
+					s.push(self.get_s(xdelta, [z-1, y, x]));
+				
+					es.push(last_es.get(&[y, x]).clone());
+					d.push(const_y_explicit_plane[y-1][x-1]);
+				}
+			}
+
+			let e = self.get_es(self.equation_to_diagonal_matrix(s, &es), d);
+			let h = get_const_terms(&e ,q).into_iter().map(|d| d * self.dy2bydx2()).collect();
+
+			e_x_expicit.push(e);
+			const_x_explicit_plane.push(h);
+		}
+
+		let e_transposed_y_expicit: Vec<Vec<_>> = (1..xdepht-1).map(|x|{
+			let mut s = vec![];
+			let mut es = vec![];
+			let mut d = vec![];
+
+			for y in 1..ydepht-1 {
+				s.push(self.get_s(ydelta, [z-1, y, x]));
+				es.push(e_x_expicit[y-1][x]);
+				d.push(const_x_explicit_plane[y-1][x-1]);
+			}
+
+			self.get_es(self.equation_to_diagonal_matrix(s, &es), d)
+
+		}).collect();
+		
+		let es_middle = matrix::transposed_vec2_to_matrix2(e_transposed_y_expicit);
+
+		let es = (0..ydepht).map(
+			|y| {
+				let mut e_middle_line = vec![];
+				for x in 0..xdepht-2 {
+					e_middle_line.push(es_middle.get(&[y,x]).clone());
+				}
+				self.insert_boundary_values(e_middle_line)
+			}
+		).collect();
+
+		matrix::vec2_to_matrix2(es)
+	}
+
 	fn dx2bydy2(&self)  -> Phasor {
 		let &[_, ydelta, xdelta] = self.grid.get_deltas();
 		Complex::new(xdelta.powf(2.0) / ydelta.powf(2.0), 0.0)
