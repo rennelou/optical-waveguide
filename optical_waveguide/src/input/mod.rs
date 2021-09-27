1a1b14adcf1e1fb1645fd2266869a19d62142af3
rennelou <rennelou@outlook.com>
2021-09-26 21:43:20 -0300

    Json entities

---
+++ optical_waveguide/src/input/mod.rs
@@ -0,0 +1,61 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct WaveguideEntity {
+    
+    
+    n0: f64,
+    n: f64,
+    
+    x_axis: Option<AxisEntity>,
+    y_axis: Option<AxisEntity>,
+    z_axis: AxisEntity,
+    
+    beams: Vec<GaussianBeamEntity>
+}
+
+#[derive(Serialize, Deserialize)]
+struct AxisEntity {
+    width: f64,
+    delta: f64
+}
+
+#[derive(Serialize, Deserialize)]
+struct GaussianBeamEntity {
+    k: f64,
+    width: f64,
+    
+    x: Option<f64>,
+    y: Option<f64>
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn happy_path() {
+        let data = r#"
+        {
+            "n0": 3.377,
+            "n": 3.38,
+            "x_axis": {
+                "width": 40,
+                "delta": 0.02  
+            },
+            "z_axis": {
+                "width": 750,
+                "delta": 0.5
+            },
+            "beams": [
+                {
+                    "k": 5.4636,
+                    "x": 20,
+                    "width": 8
+                }
+            ]
+        }"#;
+
+        let _: WaveguideEntity = serde_json::from_str(data).unwrap();
+    }
+}
--- optical_waveguide/src/lib.rs
+++ optical_waveguide/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod input;
 pub mod export;
 pub mod tools;
 pub mod fp;
